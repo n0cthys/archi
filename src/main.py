02adb2036f14d2bec4a97e8d758833c92371de1b
n0cthys <ncthys@gmail.com>
2024-01-10 15:08:32 +0100

    adding pieces, basic architect change

--- src/main.py
+++ src/main.py
@@ -1,15 +1,4 @@
-import chess
-import chess.svg
-import chess.pgn
-from IPython.display import display
-from io import StringIO
+from basegame import createbasegame
 
-board = chess.Board()                       
-pgn = open("./games/game.pgn")
-first_game = chess.pgn.read_game(pgn)
-
-board = first_game.board()
-for move in first_game.mainline_moves():
-    board.push(move)
-    
-display(board)
+# inicializal egy kulon kattintgathato tablat
+createbasegame()
